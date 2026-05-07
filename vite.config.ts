95b0cb4df94e6e8dc53758796f248b63f50310cd
sharif mohammdin <shareefmohammdin@gmail.com>
2026-05-07 02:11:00 +0200

    fixed the upload problems

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-  // استبدل 'your-repo-name' باسم مستودعك على قيت هاب بالضبط
-  base: '/your-repo-name/', 
+  plugins: [tailwindcss()],
+  base: '/password-Generator/', // ضع اسم المستودع هنا بين شرطتين مائلتين
 })
